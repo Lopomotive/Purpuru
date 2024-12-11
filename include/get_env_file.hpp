7ba3b09288514dad1eea612a5a2f67d04cc6115c
Lopomotive <mikehawk.iblm@gmail.com>
2024-12-11 21:25:24 +0100

    finnished env

--- include/get_env_file.hpp
+++ include/get_env_file.hpp
@@ -4,8 +4,10 @@
 #include <fstream> 
 #include <iostream>
 #include <regex>
+#include <unordered_map>
+#include <memory>
 
-const char* env_file = "../.env";
+constexpr char* env_file = "../.env";
 
 bool is_valid_env(const char* env_file){
   FILE *fd = fopen(env_file, "r");
@@ -16,7 +18,7 @@ bool is_valid_env(const char* env_file){
   else return true;
 }
 
-const char* get_env_values(const char* env_file){
+std::string get_env_value(const char* env_file, std::string value){
   if(is_valid_env(env_file)){
     const char* env_file_full_path = std::getenv(env_file);
   }
@@ -25,6 +27,28 @@ const char* get_env_values(const char* env_file){
     std::regex client_secret_id{"CLIENT_SECRET=([^\\s]+)"};
     std::regex token{"TOKEN=([^\\s]+)"};
   };
+
+  ValueRegex valueregex;
+  
   std::smatch match;
   std::ifstream file(env_file);
+  std::unordered_map<std::string, std::string> env_values = {
+      {"CLIENT_ID", "Placeholder"},
+      {"CLIENT_SECRET_ID", "Placeholder"},
+      {"TOKEN", "Placeholder"},
+  };
+  std::string line = nullptr;
+  //Improve this, not following DRY
+  while(std::getline(file, line)){
+    if(std::regex_match(line, match, valueregex.clientid)){
+      env_values["CLIENT_ID"] = match[1];
+    }
+    else if(std::regex_match(line, match, valueregex.client_secret_id)){
+      env_values["CLIENT_SECRET_ID"] = match[1];
+    }
+    else if(std::regex_match(line, match, valueregex.token)){
+      env_values["TOKEN"] = match[1];
+    }
+  }
+  return env_values[value];
 }
