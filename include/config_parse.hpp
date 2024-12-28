a6a1ac270df047e1223f8f91f5ade3b884ca68a9
Lopomotive <mikehawk.iblm@gmail.com>
2024-12-29 00:19:46 +0100

    config parse rewrite init

--- include/config_parse.hpp
+++ include/config_parse.hpp
@@ -2,3 +2,164 @@
 * @brief Rewritten version of config_parse (DELETE)
 * @self First read cppfs and take inspiration
 */
+#ifdef CONFIG_PARSE_HPP
+#define CONFIG_PARSE_HPP
+
+#include <regex>
+#include <variant>
+#include <stdexcept>
+#include <variant>
+#include <filesystem>
+#include <unordered_map>
+
+#if defined(LIBCONFIG)
+#include <liboconfig.h++>
+#endif
+
+/*
+* @brief config parsing only currently supports linux filesystem
+*/
+#define LINUX
+#if defined(LINUX)
+#include <sys/stat.h>
+#endif
+
+template <typename T>
+class ParseConfig;
+class Folder;
+/*
+* @brief 
+*/
+//Continue this
+class FileTypes{
+public:
+  virtual enum filetype{
+    config,
+    data,
+    cache,
+    logging, 
+    type_unknown
+  };
+  virtual ~FileTypes() = default;
+  virtual ~FileTypes() noexcept = 0;
+  static filetype detect_filetype(const std::string_view, filetype) = 0;
+  static const inline std::unorered_map<std::string_view, filetype> extension_map = {
+  {".conf", config},
+  {".data", data},
+  {".cache", cache},
+  {".log", logging}
+  };
+};
+
+//using perm_t 
+constexpr std::unordered_map<uint8_t, uint8_t> perm_to_flag = {
+    {S_IRUSR, O_RDONLY},         // Owner read
+    {S_IWUSR, O_WRONLY},         // Owner write
+    {S_IXUSR, O_RDWR},           // Owner exec
+    {S_IRWXU, O_RDWR},           // Owner all
+    {S_IRGRP, O_RDONLY},         // Group read
+    {S_IWGRP, O_WRONLY},         // Group write
+    {S_IXGRP, O_RDWR},           // Group exec
+    {S_IRWXG, O_RDWR},           // Group all
+    {S_IROTH, O_RDONLY},         // Others read
+    {S_IWOTH, O_WRONLY},         // Others write
+    {S_IXOTH, O_RDWR},           // Others exec
+    {S_IRWXO, O_RDWR},           // Others all
+    {S_IRWXU | S_IRWXG | S_IRWXO, O_RDWR} // All permissions
+};
+
+
+typedef std::filesystem::path path_t;
+
+template <typename T>
+constexpr bool is_string = std::is_same_v<T, std::string>;
+
+template <typename T>
+struct path_converter{
+};
+
+template<>
+struct path_converter<std::string_view> {
+    static constexpr bool is_convertible = true;
+    static std::filesystem::path convert(std::string_view sv) {
+        return std::filesystem::path(sv);
+    }
+};
+
+template<>
+struct path_converter<std::string>{
+  static constexpr bool is_convertible = true;
+  static std::filesystem::path convert(const std::string& sv){
+    return std::filesystem::path(sv);
+  }
+};
+
+template <typename T>
+auto safe_path_convert(const T& value) ->
+    typename std::enable_if<path_converter<T>::is_convertible, std::filesystem::path>::type {
+    return path_converter<T>::convert(value);
+}
+
+template <typename T>
+using string_or_char_t =
+ typename std::enable_if<std::is_same_v<std::decay_t<T>, const char*> ||
+                        std::is_same_v<std::decay_t<T>, std::string>, T
+ >;
+
+/*
+* check if variable have been initilzied or not
+* works by: is_initalized_v
+*/
+template<typename T>
+struct is_initialized {
+    template<typename U = T>
+    static constexpr bool value = std::is_base_of<std::monostate, U>::value && std::is_lvalue_reference<U>::value;
+};
+
+template<typename T>
+inline constexpr bool is_init_v = is_initialized<T>::value;
+
+template <typename T, typename D>
+class is_same_eval{
+
+};
+
+/*
+* @brief folder class handling folder data type 
+* @type file_t type 
+*/
+
+typename file_t = 
+class Folder{
+private:
+  struct folder_metadata const{ 
+    std::string_view name;
+    uint8_t perm;
+    FileTypes::filetype type; 
+  };
+  /*
+  * @brief template for checking that file has appropirate metadata
+  */
+  #if define(CPP_VERSION_20 || CPP_VERSION_23_OR_LATER)
+  
+  #endif
+  
+  friend std::string_view folder;
+  friend std::string_view file; 
+  friend perm_t perm;
+public:
+  //Perm may need to be worked on here(DELETE)
+  explicit Folder(std::string_view folder_) : folder(_folder), perm(){};
+  Folder() = default;
+  [[nosdiscard]] const std::string_view& getFolder() const {return folder};
+  [[nodiscard]] const perm_t getFolderPermission() const {return perm};
+  const folder_metadata assignfolder()
+}
+
+template <> 
+class ParseConfig{
+private:
+  
+}
+
+#endif
