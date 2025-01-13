2f66d2aab80c877fb2315ad2ab6467b002bfaaef
Lopomotive <mikehawk.iblm@gmail.com>
2025-01-13 23:09:03 +0100

    string tools init

---
+++ include/common/StringTools.h
@@ -0,0 +1,10 @@
+/** 
+* @brief header file for converting types into strings and back
+*/
+#include <string>
+#include <type_traits>
+
+template <typename T = int>
+constexpr auto intToString(std::string &&value) -> std::string&{
+  
+}
